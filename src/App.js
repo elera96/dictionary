445f20343b3a4614ceea0c9d92d0f148745dae38
elera96 <laurawerling39@gmail.com>
2023-08-30 14:32:18 +0200

    basic setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './book.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <img className='logo' src={logo} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
